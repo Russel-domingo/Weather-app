0f5d9692cabb715fad60536d1748c2d387593332
Russel Domingo <russeldomingo099@gmail.com>
2026-05-07 18:44:37 +0800

    Fixed

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: "./src/main.js",
 
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "docs"),
         filename: "bundle.js",
         clean: true,
     },
