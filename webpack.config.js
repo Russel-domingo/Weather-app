f78352ad31ecadc11dd646d2010a538aa1857fbe
Russel Domingo <russeldomingo099@gmail.com>
2026-05-07 18:26:12 +0800

    Finished project

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
             },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                type: "asset",
+            }
         ],
     },
 
